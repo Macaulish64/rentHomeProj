249772c8defacd1a700954accc3a3bf0ef24cf35
Mark <justfreefood@163.com>
2018-12-23 21:22:31 +0800

    Initial commit
    HHHHH

---
+++ src/main/java/com/rent/entity/House.java
@@ -0,0 +1,23 @@
+package com.rent.entity;
+
+public class House {
+    private int houseId ;
+    private int publishUserId;
+    private String cityName;
+    private String communityName;
+    private String buildingNumber;
+    private int houseType;
+    private int  houseArea;
+    private int floorNumber;
+    private int elevatorOrNot;
+    private String houseDescription;
+    private String housePhoto;
+    private float deposit;
+    private int paymentMethod;
+    private float rent;
+    private String registTime;
+    private String updateTime;
+    private int status;
+
+
+}
