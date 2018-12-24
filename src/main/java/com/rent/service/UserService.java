b17a2ecedfe73ac02d885252eb60ce0f49793559
Macaulish64 <justfreefood@163.com>
2018-12-24 21:58:16 +0800

    Add Readme.md
    Add Readme.md

--- src/main/java/com/rent/service/UserService.java
+++ src/main/java/com/rent/service/UserService.java
@@ -1,7 +1,10 @@
 package com.rent.service;
 
-public interface  UserService {
+import com.rent.entity.User;
 
+public interface  UserService {
+    //判断是否在数据库中已存在重复的用户
+    User getExistUser(User user);
 
 
 
--- src/main/java/com/rent/web/UserController.java
+++ src/main/java/com/rent/web/UserController.java
@@ -1,15 +1,26 @@
 package com.rent.web;
 
 
+import com.rent.entity.User;
+import com.rent.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    private String register(User user) {
+       User hasUser = UserService.getExistUser(user);
+
+
+
+
+    }
 
 }
