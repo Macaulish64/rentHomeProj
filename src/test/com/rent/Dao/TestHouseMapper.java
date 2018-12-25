430d1d59db14a6339e7e51bfc56ba6653e6dc63d
FishClover <FishClover@users.noreply.github.com>
2018-12-25 21:04:22 +0800

    Add files via upload

---
+++ src/test/com/rent/Dao/TestHouseMapper.java
@@ -0,0 +1,42 @@
+package com.rent;
+
+import com.rent.BaseTest;
+import com.rent.dao.HouseMapper;
+import com.rent.entity.House;
+import com.rent.entity.HouseExample;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestHouseMapper extends BaseTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private HouseMapper houseMapper;
+
+    /* */
+    @Test
+    public void insert() {
+        House newHouse=new House();
+        newHouse.setCityname("天津");
+        newHouse.setCommunityname("西城");
+        int num=houseMapper.insertSelective(newHouse);
+        System.out.println("!!!!!\n");
+    }
+
+    /* */
+    @Test
+    public void queryhouse() {
+        HouseExample newHouse = new HouseExample();
+        newHouse.createCriteria().andCitynameIn(Collections.singletonList("北京"));
+        List<House> newhouses = houseMapper.selectByExample(newHouse);
+        System.out.println(newhouses);
+    }
+
+
+}
