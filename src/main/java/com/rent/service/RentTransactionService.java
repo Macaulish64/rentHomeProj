d99a54ba91c7131019a69411e828422bc2cf20dd
FishClover <847915793@qq.com>
2019-01-05 22:13:03 +0800

    count

--- src/main/java/com/rent/service/RentTransactionService.java
+++ src/main/java/com/rent/service/RentTransactionService.java
@@ -4,6 +4,7 @@ import com.rent.entity.RentTransaction;
 import com.rent.entity.RentTransactionExample;
 
 public interface RentTransactionService {
+    int countRentTransaction(RentTransactionExample example);
     int insertRentTransaction(RentTransaction record);
     <List>RentTransaction queryRentTransaction(RentTransactionExample example);
 }
--- src/main/java/com/rent/service/impl/RentTransactionImpl.java
+++ src/main/java/com/rent/service/impl/RentTransactionImpl.java
@@ -4,13 +4,16 @@ import com.rent.dao.RentTransactionMapper;
 import com.rent.entity.RentTransaction;
 import com.rent.entity.RentTransactionExample;
 import com.rent.service.RentTransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class RentTransactionImpl implements RentTransactionService {
     @Autowired
     private RentTransactionMapper rentTransactionMapper;
+
+    @Override
+    public int countRentTransaction(RentTransactionExample example) {
+        return rentTransactionMapper.countByExample(example);
+    }
 
     @Override
     public int insertRentTransaction(RentTransaction record) {
