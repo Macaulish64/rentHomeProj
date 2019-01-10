d9e8624a17331183c0bfba91b1328867ad593298
Macaulish64 <justfreefood@163.com>
2019-01-10 22:01:13 +0800

    add jwt in person view
    add jwt in person view

--- src/main/webapp/js/personinfo/userdit.js
+++ src/main/webapp/js/personinfo/userdit.js
@@ -28,7 +28,8 @@ $(document).ready(function() {
        alert(JSON.stringify(data));
      },*/
     error:function(){
-      alert("!!!Error");
+      alert("Please Log In First");
+      $(location).attr('href', '/rentHomeProj_war/signin');
     }
   })
 });
--- src/main/webapp/js/personinfo/usersecurity.js
+++ src/main/webapp/js/personinfo/usersecurity.js
@@ -1,3 +1,29 @@
+var jwt = localStorage["jwt"];
+var name = localStorage["name"];
+console.log("输出:"+jwt);
+$(document).ready(function() {
+    $.ajax({
+        headers: {
+            Authorization : jwt,
+            'username' : name
+        },
+        type:"GET",
+        url:"http://localhost:8080/rentHomeProj_war/user/personinfo",
+        dataType:"json",
+        global:"false",
+        success:function(data) {
+            alert("!!!");
+        },
+        /* success:function(data){
+           alert(JSON.stringify(data));
+         },*/
+        error:function(){
+            alert("Please Log In First");
+            $(location).attr('href', '/rentHomeProj_war/signin');
+        }
+    })
+});
+
 $('#changepassword').click(function () {
     var oldpass=$('#oldpassword');
     var newpass1=$('#newpassword1');
