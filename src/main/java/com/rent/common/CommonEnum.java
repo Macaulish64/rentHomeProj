ba9aa4a9fd85af08b9985ff1d3a9a081d85e9690
FishClover <847915793@qq.com>
2019-01-10 16:02:10 +0800

    请求失败

--- src/main/java/com/rent/common/CommonEnum.java
+++ src/main/java/com/rent/common/CommonEnum.java
@@ -10,7 +10,7 @@ public enum CommonEnum {
   LOGIN_SUCCESS(10002, "登录成功"),
   LOGIN_LOCKED(10003, "账号被锁定,请重置密码"),
   REQUEST_SUCCESS(10003, "请求成功"),
-  REQUEST_FAILED(10004, "请求成功"),
+  REQUEST_FAILED(10004, "请求失败"),
   JWT_SECRET(10005,"XX#$%()(#*!()!KL<><MQLMNQNQJQK sdfkjsdrow32234545df>?N<:{LWPW_hisen"),
   JWT_PAYLOAD(10006,"payload"),
   JWT_MAXAGE(10007,"3600000");// 60 * 60 * 1000 = 3600000 也就是一个小时
