364f2bc404bbffe4ff2694fee551191a4c984213
FishClover <847915793@qq.com>
2019-01-10 22:26:42 +0800

    service

--- src/main/java/com/rent/service/IncomeService.java
+++ src/main/java/com/rent/service/IncomeService.java
@@ -1,17 +1,17 @@
 package com.rent.service;
 
 import com.rent.entity.Income;
-import com.rent.entity.IncomeExample;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IncomeService {
-
-    int insertIncome(Income record);
+    //插入
+    public Map<String,Object> insertIncome(Income record);
     //在已知主键（月份+地区）的情况下，更新
-    int updateIncome(Income record);
+    public Map<String,Object> updateIncome(Income record);
     //查询，从start项到end项，从0开始
-    List<Income> queryIncome(IncomeExample example, int start, int end);
+    public List<Income> queryIncome(Map<String,List> map, int start, int end);
     //计数
-    int countIncome(IncomeExample example);
+    public int queryIncomeNum(Map<String,List> map);
 }
