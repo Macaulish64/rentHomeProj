65f896aa680fa635c0387313cb2affdae6abe316
FishClover <847915793@qq.com>
2019-01-11 11:57:33 +0800

    service

--- src/main/java/com/rent/service/UserService.java
+++ src/main/java/com/rent/service/UserService.java
@@ -17,6 +17,9 @@ public interface  UserService {
     //在得知用户标识的情况下，修改用户信息
     public Map<String,Object> updateUser(User record);
 
+    //修改密码
+    public Map<String,Object> updateUserPassword(int Userid,String oldpassword,String newPassword);
+
     //在已知用户标识的情况下，查找用户
     User selectUserById(int userId);
 
