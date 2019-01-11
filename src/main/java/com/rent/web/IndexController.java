1061a35eafb14ab1c0a9272c4c0ef3e1342d24ae
dyzsr <dybest@icloud.com>
2019-01-11 15:55:03 +0800

    sew

--- src/main/java/com/rent/web/IndexController.java
+++ src/main/java/com/rent/web/IndexController.java
@@ -57,7 +57,7 @@ public class IndexController {
 	@RequestMapping("/personview/*")
 	public String personview() {
 		System.out.printf("Into person view");
-		return "templates/personinfo/personinfo";
+		return "templates/personinfo/personviewbase";
 	}
 
 	@RequestMapping("/personedit")
