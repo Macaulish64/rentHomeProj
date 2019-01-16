38ef4ef172bfee36b0895f8b3d943abd803cc166
FishClover <847915793@qq.com>
2019-01-16 22:35:40 +0800

    js

---
+++ src/main/webapp/js/admin/adminuser.js
@@ -0,0 +1,20 @@
+
+function stringUserType(data)
+{
+    if (data==0) return '普通用户';
+    if (data==1) return '管理员';
+}
+
+function stringUserLevel(data)
+{
+    if (data==0) return '一般';
+    if (data==1) return '重要';
+    if (data==2) return '钻石级';
+}
+
+function stringcardType(data)
+{
+    if (data==0) return '身份证';
+    if (data==1) return '护照';
+}
+
