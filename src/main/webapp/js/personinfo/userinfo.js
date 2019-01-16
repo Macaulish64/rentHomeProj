411802c3d0bc6b89dac891d98f9e3a4ec4ba4316
FishClover <847915793@qq.com>
2019-01-16 23:17:33 +0800

    user js

--- src/main/webapp/js/personinfo/userinfo.js
+++ src/main/webapp/js/personinfo/userinfo.js
@@ -7,21 +7,21 @@ console.log("输出:"+jwt);
 
 function stringUserType(data)
 {
-    if (data==0) return '普通用户';
-    if (data==1) return '管理员';
+    if (data===0) return '普通用户';
+    if (data===1) return '管理员';
 }
 
 function stringUserLevel(data)
 {
-    if (data==0) return '一般';
-    if (data==1) return '重要';
-    if (data==2) return '钻石级';
+    if (data===0) return '一般';
+    if (data===1) return '重要';
+    if (data===2) return '钻石级';
 }
 
 function stringcardType(data)
 {
-    if (data==0) return '身份证';
-    if (data==1) return '护照';
+    if (data===0) return '身份证';
+    if (data===1) return '护照';
 
 function personinformation(data)
 {
