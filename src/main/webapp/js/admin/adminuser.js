ae1ab925109e7c3a0bac55bd574f9dea4ea7fcff
FishClover <847915793@qq.com>
2019-01-16 22:44:41 +0800

    adminuser js

--- src/main/webapp/js/admin/adminuser.js
+++ src/main/webapp/js/admin/adminuser.js
@@ -1,3 +1,7 @@
+var jwt = window.localStorage["jwt"];
+var username = window.localStorage["username"];
+var userid=window.localStorage['userid'];
+console.log("输出:"+jwt);
 
 function stringUserType(data)
 {
@@ -18,3 +22,50 @@ function stringcardType(data)
     if (data==1) return '护照';
 }
 
+function toshowuserlist(data)
+{
+    var i;
+    if (data.length===0) return;
+    for(i=0;i<data.length;i++) {
+        $('#ownerhouselist').append(
+            '<tr>'+
+            '<td>'+data[i].userid+'</td>'+
+            '<td>'+data[i].usernickname+'</td>'+
+            '<td>'+data[i].password +'</td>'+
+            '<td>'+stringUserType(data[i].usertype) +'</td>'+
+            '<td>'+data[i].username+'</td>'+
+            '<td>'+stringcardType(data[i].housearea)+'</td>'+
+            '<td>'+data[i].idnumber+'</td>'+
+            '<td>'+data[i].phonenumber +'</td>'+
+            '<td>'+stringUserLevel(data[i].userlevel)+'</td>'+
+            '<td>'+data[i].registcity+'</td>'+
+            '<td>'+data[i].updatetime+'</td>'+
+            '<tr>'+'</tr>'
+        );
+    }
+}
+
+$(document).ready(function() {
+    $.ajax({
+        headers: {
+            Authorization : jwt,
+            'username' : username,
+            'userid' : userid
+        },
+        type:"GET",
+        url:"http://localhost:8080/rentHomeProj_war/admin/adminusrlist",
+        dataType:"json",
+        global:"false",
+        success:function(data) {
+            //   alert(data);
+            toshowhouselist(data.list);
+        },
+        /* success:function(data){
+           alert(JSON.stringify(data));
+         },*/
+        error:function(){
+            alert("Please Log In First");
+            // $(location).attr('href', '/rentHomeProj_war/signin');
+        }
+    })
+});
