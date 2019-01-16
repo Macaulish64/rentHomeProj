88aefd0c2ade7d14fd3f604ff2e7696b55c43303
dyzsr <dybest@icloud.com>
2019-01-17 01:42:16 +0800

    signup

--- src/main/webapp/js/usersignup.js
+++ src/main/webapp/js/usersignup.js
@@ -5,24 +5,29 @@ function signup() {
   http.open('POST', 'http://localhost:8080/rentHomeProj_war/user/regist', true);
   http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-  http.onreadystatechange = function (data) {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      console.log(data);
-      var json = jQuery.parseJSON(data);
+  http.onreadystatechange = function(xhttp) {
+    return function () {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        var data = jQuery.parseJSON(xhttp.responseText);
 
-      if (json.rescode === 10002) {
-        alert("Success");
-        var jwt = json.JWT;
-        // 登录成功,存储令牌到本地
-        //alert(jwt);
-        storage["jwt"] = jwt;
-        storage["username"] = json.username;
-        storage["userid"] = json.userid;
-        console.log(jwt);
-        $(location).attr('href', '/rentHomeProj_war/');
+        alert(data.rescode);
+        alert(data);
+        console.log(data);
+        // var json = jQuery.parseJSON(data);
+
+        if (data.rescode === 10003) {
+          alert("Success");
+          var jwt = data.JWT;
+          // 登录成功,存储令牌到本地
+          //alert(jwt);
+          storage["jwt"] = jwt;
+          storage["userid"] = data.userId;
+          console.log(jwt);
+          $(location).attr('href', '/rentHomeProj_war/');
+        }
       }
     }
-  };
+  } (http);
 
   http.send($('#register').serialize());
 }
